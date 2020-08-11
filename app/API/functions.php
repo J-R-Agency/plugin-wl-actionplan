48cda6e59f380a2501d3daec5dde14de71c965e0
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-08-11 18:17:56 +0100

    RETURN GOAL DISPLAY

--- app/API/functions.php
+++ app/API/functions.php
@@ -297,6 +297,8 @@ function wl_return_goal(){
 
 	if ( is_null ( $wl_repeat_check ) ) {
 
+		echo "REPEAT CHECK: " .  $wl_repeat_check ;
+		
 		$goal_counter = $goal_counter + 1 ;
 
 		$wl_return_goal = "";
